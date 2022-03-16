4957ac2b3e7ced972f2a842fd84ac2f5229f618b
Jesse Benevides <jessebenevides@gmail.com>
2022-03-16 10:24:14 -0300

    [SCHEMAS] Corrige mensagens de erro em userSchema

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -4,7 +4,7 @@ const userSchema = Joi.object({
   displayName: Joi.string().required().min(8)
     .messages({
       'any.required': '"displayName" is required|400',
-      'string.min': '"displayName" length must be at least 8 characteres long|400',
+      'string.min': '"displayName" length must be at least 8 characters long|400',
       'string.base': '"displayName" must be a string|400',
     }),
   email: Joi.string().email().required()
@@ -15,7 +15,11 @@ const userSchema = Joi.object({
   password: Joi.string().required().min(6)
     .messages({
       'any.required': '"password" is required|400',
-      'string.min': '"password" length must be at least 6 characters long|400',
+      'string.min': '"password" length must be 6 characters long|400',
+    }),
+  image: Joi.required()
+    .messages({
+      'any.required': '"image" is required|400',
     }),
 });
 
