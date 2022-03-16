e05451e04d73af67812d5c162f8af123793ac62b
Jesse Benevides <jessebenevides@gmail.com>
2022-03-15 21:54:22 -0300

    [API] Add o middleware de erro e a rota 404 ao index da API

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.use(errorMiddleware);
+
+app.all('*', (_req, res, _next) => (
+  res.status(404).json({ message: 'Route not found' })
+));
+
+app.listen(3000, () => console.log('ouvindo porta 3000!'));
