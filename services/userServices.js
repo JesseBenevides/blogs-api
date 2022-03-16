16dd31f7bb49a7df737de278fb9d6798853d4d45
Jesse Benevides <jessebenevides@gmail.com>
2022-03-16 10:26:26 -0300

    [SERVICES] [USER] Add funções create e findAll

---
+++ services/userServices.js
@@ -0,0 +1,29 @@
+const { User } = require('../models');
+const generateToken = require('../utils/generateToken');
+
+const findAll = async () => {
+  const users = await User.findAll();
+  if (!users) {
+    return { status: 404, message: 'users not found' };
+  }
+  return { status: 200, data: users };
+};
+
+const create = async ({ displayName, email, password, image }) => {
+  const alreadyExists = await User.findOne({ where: { displayName } });
+
+  if (alreadyExists) {
+    return { status: 409, message: 'User already registered' };
+  }
+
+  const newUser = await User.create({ displayName, email, password, image });
+
+  const token = generateToken({ id: newUser.id, displayName, email });
+
+  return { status: 201, data: newUser, token };
+};
+
+module.exports = {
+  findAll,
+  create,
+};
