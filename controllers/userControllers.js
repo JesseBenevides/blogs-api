e37c974f06744597afcf6fc9c9eb030b169b7c99
Jesse Benevides <jessebenevides@gmail.com>
2022-03-16 10:27:05 -0300

    [CONTROLLERS] [USER] Add funções create e findAll

---
+++ controllers/userControllers.js
@@ -0,0 +1,30 @@
+const userServices = require('../services/userServices');
+
+const findAll = async (req, res, next) => {
+    try {
+      const result = await userServices.findAll();
+      if (result.data) {
+        return res.status(result.status).json(result.data);
+      }
+      next(result);
+    } catch (error) {
+      next(error);
+    }
+};
+
+const create = async (req, res, next) => {
+  try {
+    const result = await userServices.create(req.body);
+    if (result.token) {
+      return res.status(result.status).json({ token: result.token });
+    }
+    next(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  findAll,
+  create,
+};
