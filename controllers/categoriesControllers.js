e569c41d173e160db15afcf932c38da18fa44e74
Jesse Benevides <jessebenevides@gmail.com>
2022-03-18 22:33:40 -0300

    [CONTROLLERS] [Categories] Add função findAll

--- controllers/categoriesControllers.js
+++ controllers/categoriesControllers.js
@@ -1,5 +1,17 @@
 const categoriesServices = require('../services/categoriesServices');
 
+const findAll = async (_req, res, next) => {
+  try {
+    const result = await categoriesServices.findAll();
+
+    if (result.data) return res.status(result.status).json(result.data);
+
+    next(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
 const create = async (req, res, next) => {
   try {
     const { name } = req.body;
@@ -15,4 +27,5 @@ const create = async (req, res, next) => {
 
 module.exports = {
   create,
+  findAll,
 };
