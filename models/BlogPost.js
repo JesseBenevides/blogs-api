da5d50d602730088b47090343338bbf11a7fe7ad
Jesse Benevides <jessebenevides@gmail.com>
2022-03-20 14:34:43 -0300

    [MODELS] Ajusta atributos de BlogPost

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -2,12 +2,16 @@ module.exports = (sequelize, DataTypes) => {
   const BlogPost = sequelize.define(
     'BlogPost',
     {
-      id: { type: DataTypes.INTEGER, primaryKey: true },
+      id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
       title: { type: DataTypes.STRING },
       content: { type: DataTypes.STRING, unique: true },
       userId: { type: DataTypes.INTEGER, foreignKey: true },
     },
-    { tableName: 'BlogPosts' },
+    {
+      tableName: 'BlogPosts',
+      createdAt: 'published',
+      updatedAt: 'updated',
+    },
   );
 
   BlogPost.associate = (models) => {
