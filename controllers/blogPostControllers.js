315210bc7410fa6795e49f455610c14ed1308a02
Jesse Benevides <jessebenevides@gmail.com>
2022-03-20 15:38:02 -0300

    [CONTROLLERS] [BlogPost] Add a função findAll para listar todos os posts

--- controllers/blogPostControllers.js
+++ controllers/blogPostControllers.js
@@ -1,4 +1,4 @@
-const BlogPostServices = require('../services/blogPostServices');
+const blogPostServices = require('../services/blogPostServices');
 
 const create = async (req, res, next) => {
   try {
@@ -6,7 +6,7 @@ const create = async (req, res, next) => {
     const { title, content, categoryIds } = req.body;
     const post = { title, content, userId };
 
-    const result = await BlogPostServices.create(post, categoryIds);
+    const result = await blogPostServices.create(post, categoryIds);
 
     if (result.data) return res.status(result.status).json(result.data);
 
@@ -16,6 +16,16 @@ const create = async (req, res, next) => {
   }
 };
 
+const findAll = async (_req, res, next) => {
+  try {
+    const { status, data } = await blogPostServices.findAll();
+    return res.status(status).json(data);
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   create,
+  findAll,
 };
