c5a3f56a1b946b8d760a35447ee55d6790f5809c
Jesse Benevides <jessebenevides@gmail.com>
2022-03-20 20:05:35 -0300

    [ROUTES] Add rota PUT: /post/id - atualiza um post

--- routes/blogPostRouter.js
+++ routes/blogPostRouter.js
@@ -2,6 +2,8 @@ const express = require('express');
 const validatePost = require('../middlewares/validatePost');
 const blogPostControllers = require('../controllers/blogPostControllers');
 const authMiddleware = require('../middlewares/authMiddleware');
+const validatePostUpdate = require('../middlewares/validatePostUpdate');
+const validateUserPost = require('../middlewares/validateUserPost');
 
 const blogPostRouter = express.Router();
 
@@ -22,6 +24,14 @@ blogPostRouter.get(
   '/:id',
   authMiddleware,
   blogPostControllers.findById,
+);
+
+blogPostRouter.put(
+  '/:id',
+  authMiddleware,
+  validateUserPost,
+  validatePostUpdate,
+  blogPostControllers.update,
 );
 
 module.exports = blogPostRouter;
